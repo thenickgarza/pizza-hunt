f01ef98b9cc2f00eb6136b4343ea1f7a76062de3
Nick Garza <thenickgarza14@gmail.com>
2022-03-07 22:37:47 -0600

    updated routes for /api/pizzas succesfully

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
     useNewUrlParser: true,
-    useUnifiesTopology: true
+    useUnifiedTopology: true
 });
 
 // Use this to log mongo queries being executed!
